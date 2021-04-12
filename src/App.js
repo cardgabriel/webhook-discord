42830afa3f08d8090926df9d025c15446230292a
cardgabriel <cardgabriel98@gmail.com>
2021-04-12 00:02:37 -0300

    add webhook slack & delete dependencies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import "./App.css";
-
 function App() {
-  const handlerMessage = () => {
+  const handlerMessageDiscord = () => {
     const urlWebhook =
       "https://discord.com/api/webhooks/830828807836925972/EsUEXNVESi8Xzi3e0W43pbMCTKg63XVItS96I1lJmPqvuSUPLJldt49q1dhJ8pvBoH7h";
-
     const msg = {
-      content: "Hola soy un bot",
+      content: "Hola soy un bot de Discord",
     };
-
     fetch(urlWebhook + "?wait=true", {
       method: "POST",
       headers: { "content-type": "application/json" },
+      body: JSON.stringify(msg),
+    })
+      .then((a) => a.json())
+      .then(console.log);
+  };
+
+  const handlerMessageSlack = () => {
+    const urlWebhook =
+      "https://hooks.slack.com/services/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";
+    const msg = {
+      text: "Hola soy un bot de Slack",
+    };
+    fetch(urlWebhook, {
+      method: "POST",
       body: JSON.stringify(msg),
     })
       .then((a) => a.json())
@@ -22,11 +32,17 @@ function App() {
     <div>
       <button
         onClick={() => {
-          handlerMessage();
-          console.log("enviado");
+          handlerMessageDiscord();
         }}
       >
-        Send Message
+        Send Message Discord
+      </button>
+      <button
+        onClick={() => {
+          handlerMessageSlack();
+        }}
+      >
+        Send Message Slack
       </button>
     </div>
   );
